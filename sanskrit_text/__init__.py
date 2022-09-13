2325ac25fceba982a87be98a009d6e6f8c65feea
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-09-13 22:58:43 +0530

    build(release): bump version 0.2.1 → 0.2.2

--- sanskrit_text/__init__.py
+++ sanskrit_text/__init__.py
@@ -6,7 +6,7 @@
 
 __author__ = """Hrishikesh Terdalkar"""
 __email__ = "hrishikeshrt@linuxmail.org"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __created_at__ = "Tue Apr 17 22:20:39 2018"
 
 ###############################################################################
